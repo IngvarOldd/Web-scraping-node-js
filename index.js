9704c5948dcc1e0907ba77f54cf9cdd0d30d2e52
Igor <lyushinskij.i.a@gmail.com>
2019-06-19 14:37:56 +0300

    feat: add adding data to json file

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-fetch('https://jobs.tut.by/catalog/Informacionnye-tehnologii-Internet-Telekom/Nachalnyj-uroven')
+const obj = {
+    table: []
+};
+for (let i = 0; i < 8; i++) {
+fetch(`https://jobs.tut.by/catalog/Informacionnye-tehnologii-Internet-Telekom/Nachalnyj-uroven/page-${i}`)
     .then (
         function(response) {
             return response.text();
@@ -20,13 +25,18 @@ fetch('https://jobs.tut.by/catalog/Informacionnye-tehnologii-Internet-Telekom/Na
                 .then (
                     function(body) {
                         const $ = cheerio.load(body);
-                        const name = $('h1.header').text();
-                              salary = $('p.vacancy-salary').text();
-                              experience = $('span[data-qa = "vacancy-experience"]').text();
-                              company = $('span[itemprop = "name"]').text();
-                              employment = $('p[data-qa = "vacancy-view-employment-mode"]').text();
-                        console.log(name, '|', company, '|', employment, '| Опыт:', experience, '| З/п:', salary);
+                        const name = $('h1.header').text(),
+                              salary = $('p.vacancy-salary').text(),
+                              experience = $('span[data-qa = "vacancy-experience"]').text(),
+                              company = $('span[itemprop = "name"]').text(),
+                              employment = $('p[data-qa = "vacancy-view-employment-mode"]').text(),
+                              date = $('p.vacancy-creation-time').text(); 
+                        obj.table.push({Name: name, Salary: salary});
+                        const json = JSON.stringify(obj);                               
+                        fs.writeFileSync('result.json', json);
+                        console.log(name, '|', company, '|', employment, '| Опыт:', experience, '| З/п:', salary, '|', date);
                     }
                 )
         })
     });
+}
